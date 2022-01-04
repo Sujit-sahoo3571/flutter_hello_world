093c89eab99d75a99f134e4f04971cebcbf4c903
chinku-3571 <chinku3571@gmail.com>
2022-01-04 21:49:41 +0530

    long list view using Listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/app_screens/home.dart';
 
-void main() => runApp(MaterialApp(
-      title: "Flight",
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
-    ));
+      title: "List View",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Long List View")),
+        body: getListView(),
+      ),
+    );
+  }
+}
+
+// list generate
+List<String> generate_list() {
+  var items = List<String>.generate(100, (index) => "Item $index");
+  return items;
+}
+
+// list Builder
+Widget getListView() {
+  var listItems = generate_list();
+  var listView = ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(listItems[index]),
+            onTap: () => debugPrint("${listItems[index]} was clicked "),
+          ));
+  return listView;
+}
